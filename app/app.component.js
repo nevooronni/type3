4d16dda11fcc8c2d5f3e489b55480c170ab8cf34
Neville Oronni <nevooronni@gmail.com>
2017-09-13 15:12:42 +0300

    added all files

---
+++ app/app.component.js
@@ -0,0 +1,48 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var AppComponent = (function () {
+    function AppComponent() {
+        this.tasks = [
+            new Task("Create To-Do List app.", 0),
+            new Task("Learn Kung Fu.", 1),
+            new Task("Rewatch all the Lord of the Rings movies.", 2),
+            new Task("Do the laundry.", 3)
+        ];
+    }
+    AppComponent.prototype.doStuff = function (clickedTask) {
+        if (clickedTask.done === true) {
+            alert("This task is done!");
+        }
+        else {
+            alert("This task is not completed yet! Get to work!");
+        }
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "\n  <div class=\"container\">\n    <h1>My First Angular 2 App</h1>\n    <h3 (click)=\"doStuff(currentTask)\" *ngFor=\"let currentTask of tasks\">{{ currentTask.description }}</h3>\n  </div>\n  "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+var Task = (function () {
+    function Task(description, id) {
+        this.description = description;
+        this.id = id;
+        this.done = false;
+    }
+    return Task;
+}());
+exports.Task = Task;
+//# sourceMappingURL=app.component.js.map
